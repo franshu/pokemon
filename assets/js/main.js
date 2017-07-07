3be7e80111daaae65da22c1052c94dfca7a919ca
franshu <fran.ojeda208@gmail.com>
2017-07-07 18:23:46 -0300

    change jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,29 @@
 /*recorrer la data*/
 $(document).ready(function() {
+	/* ajax que pide data de pokemones */
+	$.ajax({
+		url: 'http://pokeapi.co/media/img/',
+		type: 'GET',
+		dataType: 'json',
+		data: {"limit": '811'},
+	})
+	.done(function() {
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+
+
 	var dibujarGifs = function(data){
 		//var gif = "";
 		//var url = "";
 		data.forEach(function(element){
-			gif = element.images.downsized_large.url;
-			url = element.bitly_gif_url;
-			$("#pokemon").append(armarTemplate(gif, url));
+			var pokename = element.name;
+			$("#pokemon").append("<div class='cajapoke'><span class='pokename'>"+ pokename + "</span></div>");
 		})
 	}	
 
@@ -15,22 +32,7 @@ var armarTemplate = function(gif,url){
 		var t= "<div class='pokemon'><img src'" + gif + "'/><a href='" + url + "'>Ver más</a></div>"
 		return t;
 	}
-/* ajax que pide data de pokemones */
-$.ajax({
-	url: 'http://pokeapi.co/media/img/',
-	type: 'GET',
-	dataType: 'json',
-	data: {"limit": '811'},
-})
-.done(function() {
-	console.log("success");
-})
-.fail(function() {
-	console.log("error");
-})
-.always(function() {
-	console.log("complete");
-});
+
 
 /* ajax que pide img de pokemones 
 $.ajax({
