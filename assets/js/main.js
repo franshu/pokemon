848bbfba13bd818b73e378894b9241d47ddaa9e8
franshu <fran.ojeda208@gmail.com>
2017-07-12 22:58:54 -0300

    ajax and modal description

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,51 @@ $(document).ready(function() {
 		data: {"limit":'811'},
 	})
 
-/*hace la funcion para imprimir name and img*/
-.done(function(p) {
-	console.log("p");
-	p.results.forEach(function(elemento,imagen){
-	var pik = imagen+1;
-	$(".poke").append("<div class='col-md-3 pokemon'><img src='http://pokeapi.co/media/img/" + pik +".png'>" + elemento.name + "</div>");
-	})
-})	
+	/*hace la funcion para imprimir name and img*/
+	.done(function(p) {
+		console.log(p.results);
+		p.results.forEach(function(elemento,indice){ 
+		var pik = indice + 1;//pik se refiere a pikachu y es el id de cada pokemon
+		$(".poke").append("<div class='col-md-3 pokemon'><a href='javascript:;' data-url='" + elemento.url + "'><img src='http://pokeapi.co/media/img/" + pik +".png'></a><br>" + elemento.name + "</div>");
+		})
+	})	
 	.fail(function() {
 		console.log("error");
 	})
 	.always(function() {
 		console.log("complete");
 	});
-})
 
+});
+
+$(document).on('click', '[data-url]', function() {
+	console.log($(this).data('url'));
+	$.ajax({
+		url: $(this).data('url'),//metodo data entrega url
+		type: 'GET',
+		dataType: 'json',
+	})
+	.done(function(detalle) { 
+		var tipos = "";
+		detalle.types.forEach(function(tipo, indice) {
+			// console.log(tipo);
+			tipos = tipos + tipo.type.name + " ";
+		})
+		detalle.abilities.forEach(function(habilidad,indice){
+			console.log(habilidad)
+			$("#detalle ul").append('<li>'+  habilidad.ability.name +'</li>')
+
+		});
+		$('#detalle .modal-title').html(detalle.name);
+		$('#detalle img').attr('src', "http://pokeapi.co/media/img/" + detalle.id +".png");
+		$('#detalle').modal('show');
+		$('.altura span').html(detalle.height);
+		$('.peso span').html(detalle.weight);
+		$('.tipo span').html(tipos);
+	});
+
+	
+})
 /*	primera idea
 var dibujarGifs = function(data){
 		//var gif = "";
